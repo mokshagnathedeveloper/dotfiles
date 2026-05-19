991ad1dae448485555d6070ee9dcd111a35ddf60
mokshagnathedeveloper <mokshagnaveerandun@gmail.com>
2026-05-19 14:24:22 +0530

    Persephone's Kiss

--- fish/config.fish
+++ fish/config.fish
@@ -63,3 +63,48 @@ alias batcheck='upower -i /org/freedesktop/UPower/devices/battery_BAT0'
 alias bgm='mpv --volume=50 --save-position-on-quit --watch-later-options-add=playlist-pos,curtime Music/J-POP+Outliers/ & disown'
 alias clock-lock='sudo cpupower frequency-set -u 2.4GHz'
 alias clock-unlock='sudo cpupower frequency-set -u 4.6GHz'
+
+
+function wl-copy-file
+    if test -f $argv[1]
+        # Get the correct mime type of the file (e.g., image/png, application/pdf)
+        set mime (file --mime-type -b $argv[1])
+        # Force it into the Wayland clipboard as a proper file stream
+        wl-copy --type $mime < $argv[1]
+        echo "Copied $argv[1] to clipboard as $mime"
+    else
+        echo "File not found!"
+    end
+end
+
+
+function ex
+    if test -f $argv[1]
+        switch $argv[1]
+            case '*.tar.bz2'
+                tar -xjf $argv[1]
+            case '*.tar.gz'
+                tar -xzf $argv[1]
+            case '*.bz2'
+                bunzip2 $argv[1]
+            case '*.rar'
+                unrar x $argv[1]
+            case '*.gz'
+                gunzip $argv[1]
+            case '*.tar'
+                tar -xf $argv[1]
+            case '*.tbz2'
+                tar -xjf $argv[1]
+            case '*.tgz'
+                tar -xzf $argv[1]
+            case '*.zip'
+                unzip $argv[1]
+            case '*.7z'
+                7z x $argv[1]
+            case '*'
+                echo "'$argv[1]' cannot be extracted via ex"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
+end
