f516380857938edf9863fffa200cf34566aacedb
mokshagnathedeveloper <mokshagnaveerandun@gmail.com>
2026-05-21 13:04:49 +0530

    Persephone's Kiss

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,11 +7,29 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Global System Options
+vim.opt.clipboard = "unnamedplus" -- Sync system clipboard
+
 require("lazy").setup({
   -- Appearance
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-  { "xiyaowong/transparent.nvim" }, -- ADD THIS LINE HERE
+  { "xiyaowong/transparent.nvim" }, 
+  
+  { 
+    "nvim-lualine/lualine.nvim", 
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local status_ok, lualine = pcall(require, "lualine")
+      if not status_ok then return end
+      
+      lualine.setup({
+        options = {
+          theme = 'auto', 
+          globalstatus = true,
+        }
+      })
+    end
+  },
   
   -- Engineering Tools (LSP & Treesitter)
   { "neovim/nvim-lspconfig" },
@@ -22,6 +40,23 @@ require("lazy").setup({
   { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
 })
 
+-- Telescope keybindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
+-- Apply your colorscheme safely after Lazy sets up
+pcall(vim.cmd.colorscheme, "catppuccin-mocha")
 
-vim.opt.clipboard = "unnamedplus"
+-- Setup transparency to clear the background layers
+local trans_ok, transparent = pcall(require, "transparent")
+if trans_ok then
+  transparent.setup({
+    extra_groups = {
+      "NormalFloat", 
+      "NvimTreeNormal", 
+      "TelescopeNormal", 
+      "TelescopeBorder",
+    },
+  })
+end
