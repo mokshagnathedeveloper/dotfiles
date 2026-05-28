ed76777606a242b7ed19c0594c488be885a723b5
mokshagnathedeveloper <mokshagnaveerandun@gmail.com>
2026-05-28 13:56:47 +0530

    Persephone's Kiss

--- hypr/scripts/toggle_wallpaper.sh
+++ hypr/scripts/toggle_wallpaper.sh
@@ -13,8 +13,8 @@ then
 else
     # If it is NOT running, launch with NVIDIA offload and your optimization flags
     # Using 'env' to pass the prime render and glx vendor variables
-    nohup env __NV_PRIME_RENDER_OFFLOAD=1
-    __GLX_VENDOR_LIBRARY_NAME=nvidia
+    nohup env __NV_PRIME_RENDER_OFFLOAD=1 \
+    __GLX_VENDOR_LIBRARY_NAME=nvidia \
     SDL_AUDIODRIVER=dummy \
         linux-wallpaperengine "$WP_ROOT/$ID" \
         --wayland \
