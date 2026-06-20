4f07f3821ac92a774e3bb5ac844cf300a0a2f120
mokshagnathedeveloper <mokshagnaveerandun@gmail.com>
2026-06-20 21:37:49 +0530

    Persephone's Kiss

--- fish/config.fish
+++ fish/config.fish
@@ -63,7 +63,7 @@ alias batcheck='upower -i /org/freedesktop/UPower/devices/battery_BAT0'
 alias bgm='mpv --no-video --volume=50 --shuffle --save-position-on-quit --watch-later-options-add=playlist-pos,curtime Music/J-POP+Outliers/'
 alias clock-lock='sudo cpupower frequency-set -u 2.4GHz'
 alias clock-unlock='sudo cpupower frequency-set -u 4.6GHz'
-alias nv-lock='sudo nvidia-smi -lgc 210,800'
+alias nv-lock='sudo nvidia-smi -lgc 210,1005'
 alias nv-unlock='sudo nvidia-smi -rgc'
 
 
